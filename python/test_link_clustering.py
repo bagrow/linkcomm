7a553869dfc76553275d0a1ff53f58794de6a21d
Yong-Yeol Ahn <yongyeol@gmail.com>
2011-01-17 19:50:36 -0500

    partition density tests: trees and cliques.

--- python/test_link_clustering.py
+++ python/test_link_clustering.py
@@ -9,8 +9,20 @@ class TestLinkClustering(unittest.TestCase):
 
     def test_partition_density(self):
         '''make sure the partition density function Dc(m,n)'''
-        # single edge: (m,n) = (1,2)
-        self.assertEqual(0.0, Dc(1,2))
+        # Trees 
+        m,n = 1,2 # single link
+        self.assertEqual(0.0*m/2.0, Dc(m,n))
+
+        m,n = 3,4
+        self.assertEqual(0.0*m/2.0, Dc(m,n))
+
+        # cliques
+        m,n = 3,3
+        self.assertEqual(1.0*m/2.0, Dc(m,n))
+
+        m,n = 6,4
+        self.assertEqual(1.0*m/2.0, Dc(m,n))
+
 
 if __name__ == '__main__':
     unittest.main()
