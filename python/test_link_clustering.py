ab4eb8e880e2733b03485079d09da386247f04b0
Yong-Yeol Ahn <yongyeol@gmail.com>
2011-01-18 21:27:41 -0500

    Clean up partition density test.

--- python/test_link_clustering.py
+++ python/test_link_clustering.py
@@ -5,23 +5,17 @@ from link_clustering import *
 class TestLinkClustering(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.small_float_num = 1e-6
 
     def test_partition_density(self):
         '''make sure the partition density function Dc(m,n)'''
-        # Trees 
-        m,n = 1,2 # single link
-        self.assertEqual(0.0*m/2.0, Dc(m,n))
+        mn_pairs = [(1,2), (3,4)] # trees
+        for m,n in mn_pairs:
+            self.assertTrue(abs(0.0*m/2.0 - Dc(m,n)) < self.small_float_num)
 
-        m,n = 3,4
-        self.assertEqual(0.0*m/2.0, Dc(m,n))
-
-        # cliques
-        m,n = 3,3
-        self.assertEqual(1.0*m/2.0, Dc(m,n))
-
-        m,n = 6,4
-        self.assertEqual(1.0*m/2.0, Dc(m,n))
+        mn_pairs = [(3,3), (6,4)] # cliques
+        for m,n in mn_pairs:
+            self.assertTrue(abs(1.0*m/2.0 - Dc(m,n)) < self.small_float_num)
 
 
 if __name__ == '__main__':
