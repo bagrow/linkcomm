d99a1c57e08f48d068dc90fde30bae17a61b40ca
Yong-Yeol Ahn <yongyeol@gmail.com>
2011-02-01 15:12:31 -0500

    __init__.py added. basic information is filled in setup.py

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='linkcomm',
+        version='1.0',
+        packages=['linkcomm',],
+        license='GNU General Public License Version 3',
+        long_description=open('README.txt').read(),
+        )
